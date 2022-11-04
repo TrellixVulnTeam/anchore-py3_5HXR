72c4d718356708fdd31f072035ddf5955864134e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-04 14:40:14 +0000

    Adding tarfile member sanitization to extractall()

--- anchore/configuration.py
+++ anchore/configuration.py
@@ -205,11 +205,49 @@ class AnchoreConfiguration (object):
         if isinstance(backup_file, str):
             #module_logger.info('Restoring anchore from file %s to path: %s' % (backup_file, dest_root))
             with tarfile.TarFile.open(backup_file, mode='r:gz') as tf:
-                tf.extractall(path=dest_root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path=dest_root)
 
         else:
             #module_logger.info('Restoring anchore from file %s to path: %s' % (backup_file.name, dest_root))
             with tarfile.TarFile.open(fileobj=backup_file, mode='r:gz') as tf:
-                tf.extractall(path=dest_root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path=dest_root)
 
         return dest_root
